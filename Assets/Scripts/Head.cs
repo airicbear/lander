c5d1feb9bfc73e352870148f71b48d82a20f22f3
Eric Nguyen <tuk94307@temple.edu>
2021-11-26 13:03:52 -0500

    Add explosion effects

--- Assets/Scripts/Head.cs
+++ Assets/Scripts/Head.cs
@@ -5,10 +5,20 @@ using UnityEngine;
 public class Head : MonoBehaviour
 {
     public Rocket rocket;
+    public GameObject rocketDummy;
+    public GameObject explosion;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        GetComponent<AudioSource>().Play();
-        rocket.Reset();
+        if (!collision.gameObject.tag.Equals("Dummy")) {
+            GameObject rocketDummyClone = Instantiate(rocketDummy, rocket.transform.position, rocket.transform.rotation);
+            rocketDummyClone.GetComponent<Rigidbody2D>().AddForce(collision.contacts[0].normal * 1000.0f);
+            rocketDummyClone.GetComponent<Rigidbody2D>().angularVelocity = collision.contacts[0].normalImpulse * 1000.0f;
+            GameObject explosionClone = Instantiate(explosion, this.transform.position, this.transform.rotation);
+            Destroy(rocketDummyClone, 2.0f);
+            Destroy(explosionClone, 2.0f);
+            GetComponent<AudioSource>().Play();
+            rocket.Reset();
+        }
     }
 }
